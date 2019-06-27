567532338e36a8e970d71b8593d197aba90d7a38
ASHOK MAHARJAN <ashok997@gmail.com>
2019-06-27 17:13:08 +0000

    initial commit

---
+++ my_project/lib/cli.rb
@@ -0,0 +1,5 @@
+class CLI
+  def run
+    puts "hello from cli"
+  end
+end
---
+++ my_project/lib/environment.rb
@@ -0,0 +1,8 @@
+require_relative "./my_project/version"
+
+module My_Project
+  class Error < StandardError; end
+  # Your code goes here...
+end
+
+require_relative "./cli"
